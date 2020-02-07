c127d1415160725e3e86fd2ad92f9137435fcbb4
krast-napoli <44581068+krast-napoli@users.noreply.github.com>
2020-02-07 08:11:25 +0300

    Update stimuls.py - there was added reading and writing to csv

--- stimuls.py
+++ stimuls.py
@@ -6,23 +6,36 @@ Stimuls = [
     {'name':'Stimul3', 'num':3, 'length':20000, 'tech':False}
 ]
 
-events = """Saccade	-	0	12000	235000	223000	690.0	799.0	1357.0	695.0
-Fixation	-	0	235000	445000	210000	1355	699	0	0
-Saccade	-	1	445000	637000	192000	1357.0	699.0	622.0	318.0
-Fixation	-	1	637000	763000	126000	618	309	0	0"""
+with open ('events.tsv', newline="") as file:
+    eventIterator = iter(csv.reader(file, delimiter='	'))
 
-eventIterator = iter(csv.reader(events.split('\n'), delimiter='	'))
+    currentStimulStart = 0
+    currentStimulEnd = 0
+    currentEvent = next(eventIterator, None)
 
-currentStimulStart = 0
-currentStimulEnd = 0
-currentEvent = next(eventIterator, None)
+    for stimul in Stimuls:
+        currentStimulEnd = currentStimulStart + stimul ['length'] * 1000 #stimul length in ms, event timings in microseconds
+        while (currentEvent is not None and int(currentEvent[4]) <= currentStimulEnd):
+            if stimul['tech'] == False and int(currentEvent[3]) >= currentStimulStart:
+                print (stimul['name'] + currentEvent[0])
+                filename = stimul['name']+'.csv'
+                with open (filename, 'a') as outputfile:
+                    fieldnames = ['EventType', '-', 'EventTypeNumber', 'TimeStart', 'TimeStop', 'Duration', 'firstX', 'firstY', 'lastX', 'lastY']
+                    writer = csv.DictWriter(outputfile, fieldnames=fieldnames)
 
-for stimul in Stimuls:
-    currentStimulEnd = currentStimulStart + stimul ['length'] * 1000 #stimul length in ms, event timings in microseconds
-    while (currentEvent is not None and int(currentEvent[4]) <= currentStimulEnd):
-        if stimul['tech'] == False and int(currentEvent[3]) >= currentStimulStart:
-            print (stimul['name'] + currentEvent[0])
-        currentEvent = next(eventIterator, None)
-    if (currentEvent is None):
-        break
-    currentStimulStart = currentStimulEnd
+                    writer.writerow({
+                                     'EventType':currentEvent[0],
+                                     '-':currentEvent[1],
+                                     'EventTypeNumber':currentEvent[2],
+                                     'TimeStart':currentEvent[3],
+                                     'TimeStop':currentEvent[4],
+                                     'Duration':currentEvent[5],
+                                     'firstX':currentEvent[6],
+                                     'firstY':currentEvent[7],
+                                     'lastX':currentEvent[8],
+                                     'lastY':currentEvent[9]
+                                    })
+            currentEvent = next(eventIterator, None)
+        if (currentEvent is None):
+            break
+        currentStimulStart = currentStimulEnd
